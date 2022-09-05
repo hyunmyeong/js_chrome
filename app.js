a47e58a230fc39369deac4fd1867ba3f6f853967
hyunmyeong <tknfk@naver.com>
2022-09-05 16:00:57 +0900

    html 기본 형식

---
+++ app.js
@@ -0,0 +1,17 @@
+const a = 5;
+const b = 2;
+
+//let은 아래에서 업데이트될 수도 있는 변수를 칭할 때 사용
+let myName = "hyun";
+
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello" + myName);
+
+//위에서 설정한 let의 변수를 바꿀 때 위와 같이 변수명만 쓰고 새로운 값을 넣기
+//let은 새롭게 처음으로 변수를 설정할 때 쓰기
+myName = "doyoung";
+
+console.log("your name is " + myName);
