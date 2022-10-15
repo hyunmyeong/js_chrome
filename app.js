8dd7efe8ec24f998657db25b718ac6974245749f
hyunmyeong <tknfk@naver.com>
2022-10-15 17:14:05 +0900

    10-15 localstorage

--- app.js
+++ app.js
@@ -4,18 +4,34 @@
 
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-const link = document.querySelector("a");
+//반복되는 string은 대문자 변수로 정해놓는 것이 좋다 => 오타가 나기 쉬운 string이라는 사실을 기억 + 상기
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY ="username";
 
 function onLoginSubmit(event) {
+    //event.preventDefault 로 (submit의)기본 동작을 막기
     event.preventDefault();
-    console.log(loginInput.value);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username)
+    paintGreeting(username);
 }
 
-function handleLink(event) {
-    event.preventDefault();
-    alert("clicked!");
+function paintGreeting(username) {
+    // greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", handleLink);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    //show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    //show the greeting
+    paintGreeting(savedUsername);
+}
